828135271c9ffd9e00d875765b42b1bf01e20351
renzhongshu <45147167+renzhongshu@users.noreply.github.com>
2020-08-31 14:46:18 +0800

    Update record_update.py

--- record_update.py
+++ record_update.py
@@ -9,24 +9,23 @@ newlog_name = "new_event_check.log"
 target_str = "Warning"
 sub_str = " This is for debugging!"
 firmware_tool = "./eeupdate64e"
-def send_commands(command=None):
+def shell_commands(command=None):
         try:
-                echo_info = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                echo_stream = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 #print("echo_info:%s" %echo_info)
-                str_stream = echo_info.stdout.read()
+                str_info = echo_stream.stdout.read()
                 event_time_stream = subprocess.Popen("date", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 event_time = event_time_stream.stdout.read()
         #       print("test time:%s" %event_time)
         #       print("test time:%s" %type(event_time))
                 #无正常输出，则收集错误打印信息 
                 if str_stream == "":
-                        err_stream = echo_info.stderr.read()
-                        print("error_info:", err_stream)
-                        return event_time, err_stream#由于返回数据以元组形式返回，需转成字符类型写入
+                        err_info = echo_info.stderr.read()
+                        print("error_info:", err_info)
+                        return event_time, err_info#由于返回数据以元组形式返回，需转成字符类型写入
 
                 else:
-                        event_time = event_time_stream.stdout.read()
-                        return event_time, str_stream
+                        return event_time, str_info
         except Exception as e:
                 sys.exit(e)
                 #return err_time, err_stream
@@ -40,7 +39,7 @@ def service_status(command):
 #       test_date = (1, 2, 3)
 #       log.write(test_date)
         #log.write(send_commands("./eeupdate64e"))
-        result = send_commands(command)
+        result = shell_commands(command)
         for i in result:
                 log.write(i)
         log.close()
